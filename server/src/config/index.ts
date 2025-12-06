6e94dfdde9d8d05bae21a093d51839b6aaa58503
Matej <hozlarmatej0@gmail.com>
2025-12-07 00:05:33 +0100

    init

---
+++ server/src/config/index.ts
@@ -0,0 +1,6 @@
+import utils from "./utils";
+import discord from "./discord";
+import app from "./app.config";
+import { envMode } from "./env/env.config";
+import database from "./database";
+export default { discord, envMode, utils, app, database };
