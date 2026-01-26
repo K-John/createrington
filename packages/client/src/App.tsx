9ea595a4a0377dc711f3ff064f5ddf1b681f228c
Matej <hozlarmatej0@gmail.com>
2026-01-26 18:05:41 +0100

    Sidebar

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,14 +1,16 @@
-import { Home } from "lucide-react";
-import { Navbar } from "./components/Navbar/Navbar";
+import { Sidebar } from "./components/Sidebar";
+import { Home } from "./pages/Home/Home";
 import { AuthProvider } from "./contexts/auth";
+import "./styles/main.scss";
+import styles from "./App.module.scss";
 
 function App() {
   return (
     <AuthProvider>
-      <div className="app">
-        <Navbar />
-        <main>
-          <Home></Home>
+      <div className={styles.app}>
+        <Sidebar />
+        <main className={styles.main}>
+          <Home />
         </main>
       </div>
     </AuthProvider>
--- packages/client/src/types/scss-module.d.ts
+++ packages/client/src/types/scss-module.d.ts
@@ -7,3 +7,13 @@ declare module "*.module.css" {
   const classes: { [key: string]: string };
   export default classes;
 }
+
+declare module "*.scss" {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module "*.css" {
+  const content: Record<string, string>;
+  export default content;
+}
